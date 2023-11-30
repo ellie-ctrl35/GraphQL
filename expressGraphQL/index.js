b9e52669cf856bba6dc4a7d54d6752ff64fc7035
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-11-30 01:01:41 +0000

    setting graphql schema

--- expressGraphQL/index.js
+++ expressGraphQL/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-require('dotenv').config()
+require('dotenv').config();
+const { graphqlHTTP }= require('express-graphql')
 const port = process.env.PORT || 5000;
 const app = express();
-
-
+const schema = require('./Schema/schema')
+app.use('/graphql',graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}))
 
 app.listen(port, console.log(`Server running on ${port}`));
---
+++ expressGraphQL/sampleData.js
@@ -0,0 +1,73 @@
+const projects = [
+    {
+      id: '1',
+      clientId:'1',
+      name:'eCommerce Website',
+      description:'the websitrghed3jhbib uh34iuugbiuy ecommerce',
+      status: 'in progress'    
+    },
+    {
+        id: '2',
+        clientId:'2',
+        name:'Dating App',
+        description:'the websitrghed3jhbib uh34iuugbiuy dating app',
+        status: 'in progress' 
+    },
+    {
+        id: '3',
+      clientId:'3',
+      name:'SEO Project',
+      description:'the websitrghed3jhbib uh34iuugbiuy seo',
+      status: 'in progress' 
+    },
+    {
+        id: '4',
+      clientId:'4',
+      name:'Design Prototype',
+      description:'the websitrghed3jhbib uh34iuugbiuy design',
+      status: 'in progress' 
+    },
+    {
+        id: '5',
+        clientId:'5',
+        name:'Auction Website',
+        description:'the websitrghed3jhbib uh34iuugbiuy auction',
+        status: 'in progress' 
+    }
+];
+
+
+const clients = [
+    {
+        id: '1',
+        name:"Tony Stark",
+        email:"tony@gmail.com",
+        phone:"018-234-3445"
+    },
+    {
+        id: '2',
+        name:"Natasha Romanova",
+        email:"natasha@gmail.com",
+        phone:"018-234-3445"
+    },
+    {
+        id: '3',
+        name:"Thor Odinson",
+        email:"thor@gmail.com",
+        phone:"018-234-3445"
+    },
+    {
+        id: '4',
+        name:"steven rogers",
+        email:"steven@gmail.com",
+        phone:"018-234-3445"
+    },
+    {
+        id: '5',
+        name:"bruce banner",
+        email:"bruce@gmail.com",
+        phone:"018-234-3445"
+    }
+];
+
+module.exports = {projects,clients};
