ddac3796690029541f8fa969f11c56d0ac3dd574
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-11-30 01:42:37 +0000

    relations in graphql

--- expressGraphQL/Schema/schema.js
+++ expressGraphQL/Schema/schema.js
@@ -2,13 +2,18 @@ const { GraphQLObjectType, GraphQLID,GraphQLString, GraphQLSchema, GraphQLList, 
 const { projects,clients}= require('../sampleData'); 
 
 const ProjectType = new GraphQLObjectType({
-    name: 'Client',
+    name: 'Project',
     fields:() =>({
         id: {type: GraphQLID},
-        clientId:{type: GraphQLInt},
         name: {type:GraphQLString},
-        email: {type:GraphQLString},
-        phone: {type:GraphQLString}
+        description: {type:GraphQLString},
+        status: {type:GraphQLString},
+        client : {
+            type: ClientType,
+            resolve(parent,args){
+                return clients.find(client => client.id === parent.clientId)
+            }
+        }
     })
 });
 
@@ -25,6 +30,21 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields:{
+        
+        projects:{
+            type: new GraphQLList (ProjectType),
+            resolve(parent,args){
+                return projects;
+            }
+        },
+        project :{
+            type: ProjectType,
+            args: {id:{type:GraphQLID}},
+            resolve(parent,args){
+                return projects.find((projects) => projects.id == args.id);
+            }
+        },
+        
         clients:{
             type: new GraphQLList (ClientType),
             resolve(parent,args){
@@ -35,7 +55,7 @@ const RootQuery = new GraphQLObjectType({
             type: ClientType,
             args: {id:{type:GraphQLID}},
             resolve(parent,args){
-                return clients.find(client => client.id == args.id);
+                return clients.find((client)=> client.id == args.id);
             }
         }
     }
