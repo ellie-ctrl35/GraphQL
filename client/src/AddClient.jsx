0d5998cf840692f69a2a3cde299e20b71e43132e
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-12-02 13:32:30 +0000

    AddClient mutation

--- client/src/AddClient.jsx
+++ client/src/AddClient.jsx
@@ -1,13 +1,41 @@
-
+import { useState } from "react";
+import { ADD_CLIENT} from "../Mutations/clientMutation";
+import { GET_CLIENTS } from "../Queries/clientQueries";
+import { useMutation } from "@apollo/client";
 
 const AddClient = () => {
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
+    const [phone,setPhone]=useState('');
+
+    const [addClient] = useMutation(ADD_CLIENT,{
+        variables: { name, email, phone },
+        update(cache, { data: { addClient } }) {
+            const {clients} = cache.readQuery({query:GET_CLIENTS});
+            cache.writeQuery({
+                query:GET_CLIENTS,
+                data:{ clients: clients.concat([addClient]) },
+            }); 
+        }
+    }); 
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+      if(name===''|| email===''|| phone===''){
+        return alert('Please fill in all fields')
+      }
+      addClient(name,email,phone);
+      setName('');
+      setEmail('');
+      setPhone('');
+    };
   return (
     <div>
-      <form style={{display:"flex",flexDirection:"column",justifyContent:"space-evenly",gap:"1.4rem"}}>
-        <input type="text" placeholder="Enter name" />
-        <input type="text" placeholder="Enter email" />
-        <input type="text" placeholder="Enter phone"/>
-        <button>ADD CLIENT</button>
+      <form onSubmit={handleSubmit} style={{display:"flex",flexDirection:"column",justifyContent:"space-evenly",gap:"1.4rem"}}>
+        <input type="text" placeholder="Enter name" value={name} onChange={(e)=>setName(e.target.value )}/>
+        <input type="text" placeholder="Enter email"  value={email} onChange={(e)=>setEmail(e.target.value )}/>
+        <input type="text" placeholder="Enter phone"  value={phone} onChange={(e)=>setPhone(e.target.value )}/>
+        <button type="submit">ADD CLIENT</button>
       </form>
     </div>
   );
