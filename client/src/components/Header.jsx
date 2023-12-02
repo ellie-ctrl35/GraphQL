f223900369d66d3829ef137ae15be5e4d977cde1
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-12-02 00:35:55 +0000

    styles for navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,10 @@ import reactLogo from '../assets/react.svg'
 
 const Header = () => {
   return (
-    <div style={{background:"dodgerblue",height:"15vh",width:"100vw",margin:"0"}}>
+    <div style={{background:"dodgerblue",height:"12vh",width:"100vw",margin:"0",position:"absolute",top:"0"}}>
        <div style={{display:"flex",flexDirection:'row',height:"80%",width:"10%",justifyContent:'center',alignItems:'center'}}>
-        <img style={{height:"80%",width:"10%"}} src={reactLogo} alt="" />
-         <h1 style={{fontSize:'0.5rem'}}>Graphql CRUD</h1>
+        <img style={{height:"90%",width:"20%"}} src={reactLogo} alt="" />
+         <h1 style={{fontSize:'0.9rem'}}>Graphql CRUD</h1>
        </div>
     </div>
   )
