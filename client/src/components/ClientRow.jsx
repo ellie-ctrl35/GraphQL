c4524adec22cfee5a8d9ac3e4a652e2cd27b2376
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-12-02 01:44:25 +0000

    deleteClient Mutation

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,12 +1,16 @@
-
+import { useMutation } from "@apollo/client"
+import { DELETE_CLIENT } from "../../Mutations/clientMutation"
 const ClientRow = ({client}) => {
+    const [deleteClient] = useMutation(DELETE_CLIENT,{
+        variables:{ id : client.id},
+    } );
   return (
     <tr>
        <td>{client.name}</td>
        <td>{client.email}</td>
        <td>{client.phone}</td>
        <td>
-         <button>
+         <button onClick={deleteClient}>
            Delete
          </button>
        </td>  
